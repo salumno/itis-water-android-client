66df1f1261d967d526a53e5fdaddf738b0985bce
Melnikov Semen <sema.melnikov@gmail.com>
2018-06-03 16:05:17 +0300

    Add login page, log in data check, ticket activity and ticket manager.

---
+++ app/src/main/java/ru/kpfu/itis/water/LoginManager.kt
@@ -0,0 +1,31 @@
+package ru.kpfu.itis.water
+
+import ru.kpfu.itis.water.api.PostLoginForm
+import ru.kpfu.itis.water.api.RestAPI
+import rx.Observable
+
+/**
+ * Created by Melnikov Semyon on 03.06.18.
+ * Higher School ITIS KFU
+ */
+
+class LoginManager(private val api: RestAPI = RestAPI()) {
+    fun logIn(login: String, password: String): Observable<PostLoginForm> {
+        return Observable.create {
+            subscriber ->
+            val callResponse = api.login(login, password)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val responseBody = response.body()
+                val postLoginForm = PostLoginForm(
+                        userId = responseBody.userId
+                )
+                subscriber.onNext(postLoginForm)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
+            }
+        }
+    }
+}
